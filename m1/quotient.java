36c367b4b00bf3d31fe016f4f50d1c03375820be
Abijithhebbar <abijith.hebbar@msitprogram.net>
2018-08-29 19:10:21 +0530

    quotient.java

---
+++ m1/quotient.java
@@ -0,0 +1,13 @@
+public class quotient
+{
+	public static void main(String args[])
+	{
+		int a, b, c, d;
+		a = 10;
+		b = 12;
+		c = b / a;
+		d = b % a;
+		System.out.println("quotient is "+c);
+		System.out.println("remainder is "+d);
+	}
+}
